64a112f778738e9df108d2dffbd6a809b1a4bcd6
Jaekyung Song <jaekyuns@andrew.cmu.edu>
2023-03-20 19:59:15 -0400

    Updated v1 to use masslessParticles

--- src/mpi-simulator-v1.cpp
+++ src/mpi-simulator-v1.cpp
@@ -3,20 +3,20 @@
 #include "quad-tree.h"
 #include "timing.h"
 
-inline void simulateStep(const QuadTree &quadTree,
-                  std::vector<Particle> &particles,
+inline void simulateStep(const QuadTree<MasslessParticle> &quadTree,
+                  std::vector<MasslessParticle> &particles,
                   const StepParameters& params) {
   // Update particles for this thread
-  std::vector<Particle> result = particles;
+  std::vector<MasslessParticle> result = particles;
 
   for (int i = 0; i < particles.size(); i++)
   {
-    Particle& curParticle = result[i];
+    MasslessParticle& curParticle = result[i];
 
     Vec2 force = Vec2(0.0f, 0.0f);
-    std::vector<Particle> nearbyParticles;
+    std::vector<MasslessParticle> nearbyParticles;
     quadTree.getParticles(nearbyParticles, curParticle.position, params.cullRadius);
-    for (const Particle& nearbyP : nearbyParticles)
+    for (const MasslessParticle& nearbyP : nearbyParticles)
       force += computeForce(curParticle, nearbyP, params.cullRadius);
     curParticle = updateParticle(curParticle, force, params.deltaTime);
   }
@@ -37,15 +37,31 @@ int main(int argc, char *argv[]) {
 
   StartupOptions options = parseOptions(argc, argv);
 
-  std::vector<Particle> particles, newParticles;
-  loadFromFile(options.inputFile, particles);
+  std::vector<Particle> allParticlesMass;
+  loadFromFile(options.inputFile, allParticlesMass);
+
+  // Create massless particles
+  std::vector<float> massArray;
+  std::vector<MasslessParticle> allParticles;
+  for(int i=0; i<allParticlesMass.size(); i++)
+  {
+    massArray.push_back(allParticlesMass[i].mass);
+    MasslessParticle particle;
+    particle.position.x = allParticlesMass[i].position.x;
+    particle.position.y = allParticlesMass[i].position.y;
+    particle.velocity.x = allParticlesMass[i].velocity.x;
+    particle.velocity.y = allParticlesMass[i].velocity.y;
+    particle.id = allParticlesMass[i].id;
+    allParticles.push_back(particle);
+  }
+  MasslessParticle::massArray = massArray.data();
 
   StepParameters stepParams = getBenchmarkStepParams(options.spaceSize);
 
   // Create struct for Particle (https://hpc-tutorials.llnl.gov/mpi/derived_data_types/struct_examples/)
-  // Particle is 1 int (id) and 5 floats (mass, position, velocity)
+  // Particle is 1 int (id) and 4 floats (position, velocity)
   MPI_Aint offsets[2], lowerbound, extent;
-  MPI_Datatype particleType, oldtypes[2];
+  MPI_Datatype masslessParticleType, oldtypes[2];
   int blockcounts[2];
   // 1 Int
   offsets[0] = 0;
@@ -55,17 +71,17 @@ int main(int argc, char *argv[]) {
   MPI_Type_get_extent(MPI_INT, &lowerbound, &extent);
   offsets[1] = 1 * extent;
   oldtypes[1] = MPI_FLOAT;
-  blockcounts[1] = 5;
+  blockcounts[1] = 4;
   // Bind
-  MPI_Type_create_struct(2, blockcounts, offsets, oldtypes, &particleType);
-  MPI_Type_commit(&particleType);
+  MPI_Type_create_struct(2, blockcounts, offsets, oldtypes, &masslessParticleType);
+  MPI_Type_commit(&masslessParticleType);
 
   // Compute displacements and counts for allgatherv operation
   int particleDisplacements[nproc];
   int particleCounts[nproc];
   {
-    const int minNumParticles = particles.size()/nproc;
-    const int numParticlesRem = particles.size() % minNumParticles;
+    const int minNumParticles = allParticles.size()/nproc;
+    const int numParticlesRem = allParticles.size() % minNumParticles;
     int particleToInc = numParticlesRem;
     for(int i=0; i<nproc; i++)
     {
@@ -76,23 +92,26 @@ int main(int argc, char *argv[]) {
     }
   }
   // Set up new particles with range of interest
-  newParticles.insert(newParticles.begin(), particles.begin()+particleDisplacements[pid], particles.begin()+particleDisplacements[pid]+particleCounts[pid]);
+  std::vector<MasslessParticle> myParticles;
+  myParticles.insert(myParticles.begin(), allParticles.begin()+particleDisplacements[pid], allParticles.begin()+particleDisplacements[pid]+particleCounts[pid]);
   
+  // Set up tree
+  QuadTree<MasslessParticle> tree;
+
   // Don't change the timeing for totalSimulationTime.
   MPI_Barrier(MPI_COMM_WORLD);
   Timer totalSimulationTimer;
   for (int i = 0; i < options.numIterations; i++) {
     // Build quadtree of all particles
-    QuadTree tree;
-    QuadTree::buildQuadTree(particles, tree);
+    QuadTree<MasslessParticle>::buildQuadTree(allParticles, tree);
 
     // Update subset of particles
-    simulateStep(tree, newParticles, stepParams);
+    simulateStep(tree, myParticles, stepParams);
     
     // Share and get updates
     MPI_Allgatherv(
-      &newParticles[0], particleCounts[pid], particleType, // Send
-      &particles[0], particleCounts, particleDisplacements, particleType, // Recieve
+      &myParticles[0], particleCounts[pid], masslessParticleType, // Send
+      &allParticles[0], particleCounts, particleDisplacements, masslessParticleType, // Recieve
       MPI_COMM_WORLD);
   }
   MPI_Barrier(MPI_COMM_WORLD);
@@ -100,7 +119,20 @@ int main(int argc, char *argv[]) {
 
   if (pid == 0) {
     printf("total simulation time: %.6fs\n", totalSimulationTime);
-    saveToFile(options.outputFile, particles);
+    // Convert to particles with mass
+    allParticlesMass.clear();
+    for(int i=0; i<allParticles.size(); i++)
+    {
+      Particle particle;
+      particle.id = allParticles[i].id;
+      particle.mass = MasslessParticle::getMass(allParticles[i]);
+      particle.position.x = allParticles[i].position.x;
+      particle.position.y = allParticles[i].position.y;
+      particle.velocity.x = allParticles[i].velocity.x;
+      particle.velocity.y = allParticles[i].velocity.y;
+      allParticlesMass.push_back(particle);
+    }
+    saveToFile(options.outputFile, allParticlesMass);
   }
 
   MPI_Finalize();
